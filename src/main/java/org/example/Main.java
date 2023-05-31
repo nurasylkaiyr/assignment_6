e3ff9dbacdc79408e48ee4939462d4da8a3e6467
nurasylkaiyr <kair.nurasyl@gmail.com>
2023-05-31 23:45:26 +0600

    Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,31 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        WeightedGraph<String> graph = new WeightedGraph<>();
 
+        Vertex<String> vertexA = new Vertex<>("A");
+        Vertex<String> vertexB = new Vertex<>("B");
+        Vertex<String> vertexC = new Vertex<>("C");
+        Vertex<String> vertexD = new Vertex<>("D");
+        Vertex<String> vertexE = new Vertex<>("E");
+
+        graph.addVertex(vertexA);
+        graph.addVertex(vertexB);
+        graph.addVertex(vertexC);
+        graph.addVertex(vertexD);
+        graph.addVertex(vertexE);
+
+        graph.addEdge(vertexA, vertexB, 1.0);
+        graph.addEdge(vertexA, vertexC, 2.0);
+        graph.addEdge(vertexB, vertexD, 3.0);
+        graph.addEdge(vertexC, vertexD, 1.5);
+        graph.addEdge(vertexC, vertexE, 2.5);
+        graph.addEdge(vertexD, vertexE, 1.0);
+
+        graph.printGraph();
+
+        Search<String> bfs = new BreadthFirstSearch<>(graph);
+        System.out.println("BFS: ");
+        bfs.search(vertexA);
     }
 }
